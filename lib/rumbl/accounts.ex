75428e0714627ed23d961ca376bf3c058630eb37
Kevin Ndung'u <kevgathuku@gmail.com>
2020-03-16 17:33:40 +0300

    Add user passwords with hashing

--- lib/rumbl/accounts.ex
+++ lib/rumbl/accounts.ex
@@ -25,9 +25,19 @@ defmodule Rumbl.Accounts do
     User.changeset(user, %{})
   end
 
+  def change_registration(%User{} = user, params) do
+    User.registration_changeset(user, params)
+  end
+
   def create_user(attrs \\ %{}) do
     %User{}
     |> User.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def register_user(attrs \\ %{}) do
+    %User{}
+    |> User.registration_changeset(attrs)
     |> Repo.insert()
   end
 end
--- lib/rumbl/accounts/user.ex
+++ lib/rumbl/accounts/user.ex
@@ -1,10 +1,13 @@
 defmodule Rumbl.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
+  require Pbkdf2
 
   schema "users" do
     field :name, :string
     field :username, :string
+    field :password_hash, :string
+    field :password, :string, virtual: true
 
     timestamps()
   end
@@ -14,5 +17,23 @@ defmodule Rumbl.Accounts.User do
     |> cast(attrs, [:name, :username])
     |> validate_required([:name, :username])
     |> validate_length(:username, min: 1, max: 20)
+  end
+
+  def registration_changeset(user, params) do
+    user
+    |> changeset(params)
+    |> cast(params, [:password], [])
+    |> validate_length(:password, min: 6, max: 120)
+    |> put_pass_hash()
+  end
+
+  defp put_pass_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_hash, Pbkdf2.hash_pwd_salt(pass))
+
+      _ ->
+        changeset
+    end
   end
 end
