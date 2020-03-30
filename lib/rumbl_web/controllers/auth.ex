d31c7dc3d50f01ad1e186a58aadfcdca2d2fe6ba
Kevin Ndung'u <kevgathuku@gmail.com>
2020-03-30 14:46:49 +0300

    Implement logout / session delete

--- lib/rumbl_web/controllers/auth.ex
+++ lib/rumbl_web/controllers/auth.ex
@@ -10,10 +10,17 @@ defmodule RumblWeb.Auth do
     assign(conn, :current_user, user)
   end
 
+  # Login / logout methods - regular functions
+  # Not part of the plug - just convenience methods
   def login(conn, user) do
     conn
     |> assign(:current_user, user)
     |> put_session(:user_id, user.id)
     |> configure_session(renew: true)
+  end
+
+  def logout(conn) do
+    #  drops the session, a session cookie will not be included in the response
+    configure_session(conn, drop: true)
   end
 end
